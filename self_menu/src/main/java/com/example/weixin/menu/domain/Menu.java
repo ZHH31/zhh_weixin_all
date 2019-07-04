0fa2bc3c84489c7f408ef098616df3873689ed53
ZHH <ZHH@HaHa-ZHH>
2019-07-05 03:02:17 +0800

    213

--- self_menu/src/main/java/com/example/weixin/menu/domain/Menu.java
+++ self_menu/src/main/java/com/example/weixin/menu/domain/Menu.java
@@ -15,6 +15,7 @@ import javax.persistence.Transient;
 
 import org.hibernate.annotations.GenericGenerator;
 
+
 @Entity
 @Table(name = "wx_self_menu_button")
 public class Menu {
@@ -28,15 +29,26 @@ public class Menu {
 	@JoinColumn(name = "parent_id")
 	private List<Menu> subMenus = new LinkedList<>();
 
+	/** 菜单的响应动作类型，view表示网页类型，click表示点击类型，miniprogram表示小程序类型 */
 	private String type;
+	/** 菜单标题，不超过16个字节，子菜单不超过60个字节 */
 	private String name;
+	/** 菜单KEY值，用于消息接口推送，不超过128字节 */
+	// key是一个关键字，必须要使用反单引号括起来
 	@Column(name = "`key`")
 	private String key;
+	/** 网页 链接，用户点击菜单可打开链接，不超过1024字节。 type为miniprogram时，不支持小程序的老版本客户端将打开本url。 */
 	private String url;
+	/** 调用新增永久素材接口返回的合法media_id */
 	private String mediaId;
-	private String appId;	
+	/** 小程序的appid（仅认证公众号可配置） */
+	private String appId;
+	/** 小程序的页面路径 */
+	
 	private String pagePath;
+	@Transient
 	private boolean active;
+	@Transient
 	private boolean show;
 	
 	
